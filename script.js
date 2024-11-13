3fc87e56b923807d330049ff7680a430ec03c402
CodeManipulator64x <sakiinoshark@gmail.com>
2024-11-13 12:01:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+const ayarlarButton = document.getElementById("ayarlarButton");
+const ayarlarBolumu = document.getElementById("ayarlarBolumu");
+const anaIcerik = document.getElementById("anaIcerik");
 
+ayarlarButton.addEventListener("click", () => {
+  ayarlarBolumu.style.display = "block";
+  anaIcerik.style.display = "none";
+});
